d84009abd6fa29839a80b176c40a0035bf532168
ypradeeprao <39065049+ypradeeprao@users.noreply.github.com>
2023-02-14 04:39:30 +0000

    dfd

--- client/src/App.js
+++ client/src/App.js
@@ -53,7 +53,7 @@ state = {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Reactttt</h1>
         </header>
         <p className="App-intro">{this.state.data}</p>
       </div>
